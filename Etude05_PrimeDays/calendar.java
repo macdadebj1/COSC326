8f04f952267a5b7c2e4857b24802364bdf727c01
Blake <macdadeblake@gmail.com>
2021-07-13 20:57:06 +1200

    First working draft

--- Etude05_PrimeDays/calendar.java
+++ Etude05_PrimeDays/calendar.java
@@ -1,15 +1,28 @@
 import java.util.Scanner;
 import java.lang.Math;
 
+//Is prime if day of month is prime, month is prime, and day of year is prime...
+//output: <number of day>: <number of month> <day of month>
+
 public class calendar{
     public static void main(String[] args){
-        if(args.length <= 0){
-            System.out.println("Please provide input data!");
-            
-        } else{
-            for(String nums : args){
-                System.out.println(nums + " " + isPrime(Integer.parseInt(nums)));
-            } 
+        int yearLength = 0;
+        int dayOfYear = 0;
+        if(args.length > 0){
+            for(String monthLenghts: args){
+                try {
+                    yearLength += Integer.parseInt(monthLenghts);
+                } catch (NumberFormatException e){
+                    System.out.println(e+" in main while trying to parse integers from command line!");
+                }
+            }
+            System.out.println("Year Length: "+yearLength);
+            for(int month = 0; month < args.length; month++){
+                for(int day = 0; day < Integer.parseInt(args[month]); day++){
+                    if(isPrime(day+1)&&isPrime(month+1)&&isPrime(dayOfYear+1)) System.out.println((dayOfYear+1)+": "+(month+1)+ " "+(day+1));
+                    dayOfYear++;
+                }
+            }
         }
         
     }
@@ -17,14 +30,17 @@ public class calendar{
 
     public static boolean isPrime(int number){
         //for(int i = 2; i < number ** 0.5; i++
+        if(number ==0) return false;
+        if(number == 1) return false;
         if(number == 2 || number ==3) return true;
-        if(Math.pow(number,0.5) %2 == 0) return false;
-        for(int i = 1; i < number; i++){
-            if(number % i != 0){}
+        int i = 2;
+        while(i*i <= number){
+            if((number % i) ==0) return false;
+            i++;
         }
-        return false;
+        return true;
     }
     public static boolean eratosthenes(int num){
-         
+         return false;
     }
 }
