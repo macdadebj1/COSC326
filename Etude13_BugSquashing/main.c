903627e1188b993ed31255478aff388e62f6a839
Blake <macdadeblake@gmail.com>
2021-08-29 14:06:04 +1200

    more changes to main.c

--- Etude13_BugSquashing/main.c
+++ Etude13_BugSquashing/main.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 struct S{
-    char *firstName;
+    char* firstName;
     char* lastName; // inconsistent * placement.
     int phone;
     char* emailAddress;
@@ -79,13 +79,13 @@ void sph(struct S** ss){
 
 int findPhoneNumber(struct S** ss, int s){
     while(++i < count){
-        if(ss[i]->phone == ss)
+        if(ss[i]->phone == s) //changed so is comparing correctly
             return 1;
     }
     return 0;
 }
 
-int main(int argc, char ** argv){ //inconsistent ** placement...
+int main(int argc, char** argv){ //inconsistent ** placement...
     int i;
     int count = 0;
     char buffer[10]; //incorrect declaration..? maybe?
@@ -93,7 +93,17 @@ int main(int argc, char ** argv){ //inconsistent ** placement...
     struct S** ss = (struct S**) malloc (100*sizeof(struct S**));
     struct S* s = malloc(sizeof(*s));
 
-    FILE *f = fopen(argv[1],"r");
+    printf("Number of arguments recieved: %d\n",argc);
+    if(argc != 2){
+        printf("Incorrect number of arguments supplied! Got %d\n",argc);
+    }
+    FILE* f = fopen(argv[1],"r"); // no error checking
+    if(f == NULL){
+        printf("\n the file %s could not be opened!",argv[1]);
+        exit(1);
+    }else{
+        printf("File isn't null!");
+    }
 
     for(i = 0; i < 50; i++){
         s->firstName = (char*) malloc(80 * sizeof(s->firstName[0]));
@@ -101,41 +111,41 @@ int main(int argc, char ** argv){ //inconsistent ** placement...
         s->emailAddress = (char*) malloc(80 * sizeof(s->firstName[0])); //May go out of bounds...
 
         //fscanf is depreciated... use fscanf_s instead...
-        fscanf(f, "%s %s %d %s", &s->firstName,&s->lastName, &s->phone,&s->emailAddress);
+        fscanf(f, "%79s %79s %d %79s", s->firstName,s->lastName, &s->phone,s->emailAddress);
 
         ss[count] = s;
         count+=1;
-        {
-            int command = 10;
-            while(command != 0){ //may be a dangerous loop...
-                char* val = malloc(100*sizeof(val[0]));
-                gets(buffer); //gets is INSECURE!
-                command = atoi(buffer);
-                gets(buffer);
-                strcpy(val,buffer);
-                switch(command){
-                    case 1:
-                        printf("looking for email %s\n",val);
-                        sem(ss);
-                        printf("found it? %d\n",findEmail(ss,val));
-                        break;
-                    case 2:
-                        printf("looking for firstname %s\n",val);
-                        sfn(ss);
-                        printf("found it? %d\n",findFirstName(ss,val));
-                        break;
-                    case 3:
-                        printf("looking for lastname %s\n",val);
-                        sln(ss);
-                        printf("found it? %d\n",findLastName(ss,val));
-                        break;
-                    case 4:
-                        printf("looking for email %s\n",val); //should be looking for ph number.
-                        sph(ss);
-                        printf("found it? %d\n",findPhoneNumber(ss,atoi(val)));
-                    default:
-                        break;
-                }
+
+        printf("\nin for loop!");
+        int command = 10;
+        while(command != 0){ //may be a dangerous loop...
+            char* val = malloc(100*sizeof(val[0]));
+            gets(buffer); //gets is INSECURE!
+            command = atoi(buffer);
+            gets(buffer);
+            strcpy(val,buffer);
+            switch(command){
+                case 1:
+                    printf("looking for email %s\n",val);
+                    sem(ss);
+                    printf("found it? %d\n",findEmail(ss,val));
+                    break;
+                case 2:
+                    printf("looking for firstname %s\n",val);
+                    sfn(ss);
+                    printf("found it? %d\n",findFirstName(ss,val));
+                    break;
+                case 3:
+                    printf("looking for lastname %s\n",val);
+                    sln(ss);
+                    printf("found it? %d\n",findLastName(ss,val));
+                    break;
+                case 4:
+                    printf("looking for email %s\n",val); //should be looking for ph number.
+                    sph(ss);
+                    printf("found it? %d\n",findPhoneNumber(ss,atoi(val)));
+                default:
+                    break;
             }
         }
 
